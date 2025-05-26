b600cc7c2d5888d4768c352605a4e02d30d855b2
Turi020 <turancan.dur@student.htldornbirn.at>
2025-05-26 21:11:10 +0200

    2.Tag Codinggame Beispiele

---
+++ src/CodingGame Beispiel/2.Tag/2.Aufgabe/script.js
@@ -0,0 +1,41 @@
+/*The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+01 Test 1
+Input
+Expected output
+6
+3600
+02 Test 2
+Input
+Expected output
+12
+14400
+03 Test 3
+Input
+Expected output
+20
+40000
+04 Test 4
+Input
+Expected output
+1
+100
+05 Test 5
+Input
+Expected output
+9
+8100
+*/
+
+
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n = parseInt(readline());
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log(n*(n*100));
