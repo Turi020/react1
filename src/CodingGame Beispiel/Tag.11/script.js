84463cdfca035dc3e4b8683755787f7c0219234f
Turi020 <turancan.dur@student.htldornbirn.at>
2025-06-06 12:04:00 +0200

    11.Tag Codingbeispiel done

---
+++ src/CodingGame Beispiel/Tag.11/script.js
@@ -0,0 +1,28 @@
+/*Find the hypotenuse of a right triangle given the length of the legs. Round to the nearest integer.
+Input
+Line 1:length of leg_1
+Line 2:length of leg_2
+Output
+length of the hypotenuse
+Constraints
+1<=leg_1<=25
+1<=leg_2<=25
+Example
+Input
+2
+3
+Output
+4
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const leg1 = parseInt(readline());
+const leg2 = parseInt(readline());
+const hypotenuse = Math.sqrt(leg1*leg1+leg2*leg2)
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log(Math.round(hypotenuse));
