f35fa74c19c9eca7a6fbb294f382c3248e8ccb1d
Turi020 <turancan.dur@student.htldornbirn.at>
2025-06-12 01:07:33 +0200

    Tag.14 Commits

---
+++ src/CodingGame Beispiel/Tag.14/script.js
@@ -0,0 +1,19 @@
+let zahlen = [5, 12, 7, 18, 3, 25];
+
+for (let i = 0; i < zahlen.length; i++) {
+  if (zahlen[i] > 10) {
+    console.log(zahlen[i]);
+  }
+}
+
+
+
+/*------------------------------*/
+
+
+let number =[123,53,12,91,94,130];
+for( let i=0; i<number.length;i++){
+    if(80>number[i]){
+        console.log(number[i]);
+    }
+}
