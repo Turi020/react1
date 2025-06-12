2a40e0dadd39a1818b13104f082f76f0a45197d5
Turi020 <turancan.dur@student.htldornbirn.at>
2025-06-12 23:51:35 +0200

    Tag.15 Commits

---
+++ src/CodingGame Beispiel/Tag.15/2aufgabescript.js
@@ -0,0 +1,8 @@
+let namen = ["Johannes", "Zeki", "Raymond","Ali", "Lea", "Turi", "Steve", "Ken"];
+
+for(let i=0; i<namen.length;i++){
+    if(namen[i].length>=5){
+       console.log(namen[i])
+    }
+
+}
---
+++ src/CodingGame Beispiel/Tag.15/script.js
@@ -0,0 +1,11 @@
+let sum=0;
+for(let i=1;i<=10;i++){
+    if(i%2==0){
+        console.log(i);
+        sum+=i;
+}
+
+}console.log("Das endresultat beträgt " + sum);
+
+
+/*-------------------------*/
