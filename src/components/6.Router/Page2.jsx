d32ccac6caee21340f6f4f57231c531d0516c28e
Turi020 <turancan.dur@student.htldornbirn.at>
2025-06-12 21:40:28 +0200

    Aufgabe Router 6 weitergemacht

--- src/components/6.Router/Page2.jsx
+++ src/components/6.Router/Page2.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from 'react-router';
+
+
 
 export default function Page2() {
+  const location = useLocation();
+  const person = location.state.person;
+  console.log(person);
+  console.log(location);
+  
+  
+
   return (
     <div>
-        <h1>Page 2 </h1>
-        <Link to="/" className="hover:cursor-pointer">Page1</Link>;
+        <h1>Page 2xxs</h1>
+        <div>Hello{person.firstname}</div>
+        <Link to="/" className="hover:cursor-pointer">
+        Page1
+        </Link>
     </div>
-  )
+  );
 }
