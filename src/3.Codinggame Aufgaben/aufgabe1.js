4a4badf105fedc40e89f15263cb907ca63d673e4
Turi020 <turancan.dur@student.htldornbirn.at>
2025-06-15 23:47:25 +0200

    Codinggame HÜ 1

---
+++ src/3.Codinggame Aufgaben/aufgabe1.js
@@ -0,0 +1,15 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+for (let i = 1; i < 4; i++) {
+
+    if(i%2==0){
+        console.log(i*2);
+    }
+    else(console.log(i*3))
+    }
+    
+    // Write an answer using console.log()
+    // To debug: console.error('Debug messages...');
