6dc66fd67e8287eb2c95c7e29945d5dae108cc19
Turi020 <turancan.dur@student.htldornbirn.at>
2025-06-21 01:12:20 +0200

    15.tag for loop aufgaben

---
+++ src/CodingGame Beispiel/15.Tag/script.js
@@ -0,0 +1,13 @@
+for(let i=1;i<=44;i++){
+    if(i%12==0&&i%6==0){
+        console.log("Bau mir ein Haus")
+    }else if(
+        i%3==0
+    ){
+        console.log("Haus")
+    }
+    else if( i%7==0){
+        console.log("Bau mir ein")
+    }
+    else(console.log(i));
+}
